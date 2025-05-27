1bb6b34436194a4510ec334a24540c7c506b4c8e
Your Name <you@example.com>
2025-05-27 13:55:38 +0300

    added

--- my-app/src/components/user-cards/UserCards.jsx
+++ my-app/src/components/user-cards/UserCards.jsx
@@ -5,15 +5,22 @@ import { users } from '../users/data';
 export default function UserCards() {
     const [userCards, setUserCards] = useState(users);
 
-    console.log(userCards)
-
     return (
         <div className='UserCards'>
             {userCards.map(x =>
                 <div className='card'>
                     <header>{x.firstName} {x.lastName}</header>
 
-                    
+                    <p>
+                        {x.phone}
+                        <a href={`tel:${x.phone}`}><i className='fa fa-phone'></i></a>
+                        <a href={`https://wa.me/${x.phone}`}><i className='fa fa-whatsapp'></i></a>
+                    </p>
+                    <p>
+                        {x.email}
+                        <a href={`mailto:${x.email}`}><i className='fa fa-envelope'></i></a>
+                    </p>
+                    <p><b>תאריך לידה:</b> {x.birthday}</p>
                 </div>
             )}
         </div>
