9713d547c915f512672ddaf37faf9187f8a38a23
Your Name <you@example.com>
2025-06-03 11:55:43 +0300

    added

--- react-api/src/components/Contact.jsx
+++ react-api/src/components/Contact.jsx
@@ -1,5 +1,34 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 export default function Contact() {
+    const [contacts, setContacts] = useState([]);
+    
+    // useEffect(() => {
+    //     fetch(`https://api.shipap.co.il/contact`)
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         setContacts(data);
+    //     });
+    // }, []);
+
+    const getTickets = async () => {
+        const res = await fetch(`https://api.shipap.co.il/contact`);
+        const data = await res.json();
+        setContacts(data);
+    };
+
+    useEffect(() => {
+        getTickets();
+    }, []);
+
     return (
-        <h1>Contact</h1>
+        <>
+            <h1>Contact</h1>
+
+            <ul>
+                {contacts.map(x => <li style={{color: 'white'}}>{x.fullName}</li>)}
+            </ul>
+        </>
     )
 }
