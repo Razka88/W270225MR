0c06bc535550fd7630dedb782f7799e2cfcf6328
Your Name <you@example.com>
2025-06-03 15:49:38 +0300

    added

--- react-api/src/components/ContactForm.jsx
+++ react-api/src/components/ContactForm.jsx
@@ -1,4 +1,22 @@
+import { useState } from "react"
+
 export default function ContactForm() {
+    const [formData, setFormData] = useState({
+        fullName: '',
+        phone: '',
+        email: '',
+        message: '',
+    });
+
+    const dataChange = ev => {
+        const { id, value } = ev.target;
+
+        setFormData({
+            ...formData,
+            [id]: value,
+        });
+    }
+
     return (
         <>
             <h1>ContactForm</h1>
@@ -6,22 +24,22 @@ export default function ContactForm() {
             <form>
                 <label>
                     שם הפונה:
-                    <input type="text" id="fullName" />
+                    <input type="text" id="fullName" onChange={dataChange} />
                 </label>
 
                 <label>
                     טלפון:
-                    <input type="tel" id="phone" />
+                    <input type="tel" id="phone" value={formData.phone} onChange={dataChange} />
                 </label>
 
                 <label>
                     אימייל:
-                    <input type="email" id="email" />
+                    <input type="email" id="email" value={formData.email} onChange={dataChange} />
                 </label>
 
                 <label>
                     תוכן הפנייה:
-                    <textarea name="" id="message" cols="30" rows="5"></textarea>
+                    <textarea name="" id="message" cols="30" rows="5" value={formData.message} onChange={dataChange}></textarea>
                 </label>
 
                 <button>שלח</button>
