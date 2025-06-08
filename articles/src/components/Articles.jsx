e6d1d655e61a39cda2a78762390de8a7cd01c914
Your Name <you@example.com>
2025-06-08 15:58:36 +0300

    added

--- articles/src/components/Articles.jsx
+++ articles/src/components/Articles.jsx
@@ -1,5 +1,55 @@
+import { useEffect, useState } from "react";
+import moment from 'moment';
+
 export default function Articles() {
+    const [articles, setArticles] = useState([]);
+
+    const getData = async () => {
+        const res = await fetch(`https://api.shipap.co.il/articles`, {
+            credentials: 'include',
+        });
+
+        if (res.ok) {
+            const data = await res.json();
+            setArticles(data);
+        }
+    }
+    
+    useEffect(() => {
+        getData();
+    }, []);
+
     return (
-        <div>Articles</div>
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>כותרת</th>
+                        <th>תאריך יצירה</th>
+                        <th>תאריך פרסום</th>
+                        <th>צפיות</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        articles.map((art, i) => 
+                            <tr key={art.id}>
+                                <td>{i + 1}</td>
+                                <td>{art.headline}</td>
+                                <td>{moment(art.addedTime).format("DD/MM/YY")}</td>
+                                <td>{moment(art.publishDate).format("DD/MM/YY")}</td>
+                                <td>{art.views}</td>
+                                <td>
+                                    <button className="green"><i className="fa fa-edit"></i></button>
+                                    <button className="red"><i className="fa fa-trash"></i></button>
+                                </td>
+                            </tr>    
+                        )
+                    }
+                </tbody>
+            </table>
+        </div>
     )
 }
