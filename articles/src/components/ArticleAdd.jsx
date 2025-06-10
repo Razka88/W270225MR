bae7e6ed4a693be2fa5b6fe562a40b8778591b90
Your Name <you@example.com>
2025-06-10 12:31:45 +0300

    added

--- articles/src/components/ArticleAdd.jsx
+++ articles/src/components/ArticleAdd.jsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
 import { Link } from "react-router";
 
 export default function ArticleAdd() {
+    const [form, setForm] = useState({
+        headline: '',
+        description: '',
+        content: '',
+        imgUrl: '',
+        publishDate: '',
+    });
 
     const add = async ev => {
         ev.preventDefault();
 
+        console.log(form);
+    }
 
+    const change = ev => {
+        const { id, value } = ev.target;
+
+        setForm({
+            ...form,
+            [id]: value,
+        });
     }
     
     return (
@@ -20,27 +37,27 @@ export default function ArticleAdd() {
             <form onSubmit={add}>
                 <label>
                     כותרת:
-                    <input type="text" id="headline" />
+                    <input type="text" id="headline" value={form.headline} onChange={change} />
                 </label>
 
                 <label>
                     תיאור:
-                    <textarea id="description" cols="30" rows="5"></textarea>
+                    <textarea id="description" value={form.description} onChange={change} cols="30" rows="5"></textarea>
                 </label>
 
                 <label>
                     תוכן:
-                    <textarea id="content" cols="30" rows="10"></textarea>
+                    <textarea id="content" value={form.content} onChange={change} cols="30" rows="10"></textarea>
                 </label>
 
                 <label>
                     תאריך פרסום:
-                    <input type="date" id="publishDate" />
+                    <input type="date" id="publishDate" value={form.publishDate} onChange={change} />
                 </label>
 
                 <label>
                     קישור לתמונה:
-                    <input type="text" id="imgUrl" />
+                    <input type="text" id="imgUrl" value={form.imgUrl} onChange={change} />
                 </label>
 
                 <button>שמור <i className="fa fa-save"></i></button>
