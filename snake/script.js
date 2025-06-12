81d27c839c078c476972140f64d057f7e7b72691
Your Name <you@example.com>
2025-06-12 15:48:55 +0300

    added

--- snake/script.js
+++ snake/script.js
@@ -1,20 +1,39 @@
 const height = 40;
 const width = 30;
 const snake = [4, 3, 2, 1, 0];
+const board = document.querySelector('.board');
+board.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
+const divs = [];
 
 function createBoard() {
-    const board = document.querySelector('.board');
-    board.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
-
     for (let i = 0; i < width * height; i++) {
         const div = document.createElement("div");
         div.innerText = i;
         board.appendChild(div);
+        divs.push(div);
     }
+
+    color();
 }
 
 function color() {
-    
+    divs.forEach(div => div.classList.remove('active'));
+
+    snake.forEach(num => {
+        divs[num].classList.add('active');
+    });
+}
+
+function move(dir) {
+    let head = snake[0];
+
+    if (dir === 'left') {
+        head++;
+    }
+
+    snake.unshift(head);
+    snake.pop();
+    color();
 }
 
 createBoard();
