950a9fe9880d2c1280f9cd5afb2f54bd218a4790
Your Name <you@example.com>
2025-06-15 12:18:45 +0300

    added

--- articles/src/components/Signup.jsx
+++ articles/src/components/Signup.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react"
+import { Link } from "react-router";
 
 export default function Signup() {
     const [form, setForm] = useState({
+        fullName: '',
+        email: '',
         userName: '',
         password: '',
-        email: '',
-        fullName: '',
     });
 
 
@@ -17,12 +18,28 @@ export default function Signup() {
 
             <form>
                 <label>
-                    שם מלא:
+                    <i className="fa fa-user"></i> שם מלא:
                     <input type="text" id="fullName" value={form.fullName} />
                 </label>
 
+                <label>
+                    <i className="fa fa-envelope"></i> אימייל:
+                    <input type="text" id="email" value={form.email} />
+                </label>
+
+                <label>
+                <i className="fa fa-address-card"></i> שם משתמש:
+                    <input type="text" id="userName" value={form.userName} />
+                </label>
+
+                <label>
+                    <i className="fa fa-asterisk"></i> סיסמה:
+                    <input type="password" id="password" value={form.password} />
+                </label>
+                <button>הרשם</button>
             </form>
             
+            <Link to="/">להתחברות לחץ כאן</Link>
         </>
     )
 }
