86c39f5b45e4ea2fcc9cdbc1a476cc216a1844de
Your Name <you@example.com>
2025-06-22 13:51:20 +0300

    added

--- nodejs/front-for-node/src/Users.jsx
+++ nodejs/front-for-node/src/Users.jsx
@@ -1,7 +1,47 @@
+import { useEffect, useState } from "react";
+
 export default function Users() {
+    const [users, setUsers] = useState([]);
+
+    const getUsers = async () => {
+        const res = await fetch("http://localhost:3000/users");
+
+        if (res.ok) {
+            const data = await res.json();
+            setUsers(data);
+        } else {
+            console.log(res.status);
+        }
+    }
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+
     return (
         <div>
             <h1>Users</h1>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>שם פרטי</th>
+                        <th>שם משפחה</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        users.map((u, i) =>
+                            <tr>
+                                <td>{i + 1}</td>
+                                <td>{u.firstName}</td>
+                                <td>{u.lastName}</td>
+                            </tr>
+                        )
+                    }
+                </tbody>
+            </table>
         </div>
     )
 }
