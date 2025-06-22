0d2be8b80bd53a3b462aaebd1977ea1d8e45106c
Your Name <you@example.com>
2025-06-22 11:49:18 +0300

    added

---
+++ nodejs/hello-nodejs/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+// חיבור למסד הנתונים
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/full-stack-W270225MR');
+    console.log('mongodb connection');
+}
+
+main().catch(err => console.log(err));
+
+// שימוש ב-Express
+const app = express();
+
+// הפעלת השרת ושימוש בפורט
+app.listen(3000, () => {
+    console.log("listening on port 3000");
+});
+
+// יירוט בקשה ללא ניתוב (דף הבית)
+app.get('/', (req, res) => {
+    res.send({
+        message: "Hello world!",
+    });
+});
+
+// יירוט בקשה עם ניתוב /users
+app.get("/users", (req, res) => {
+    res.send([
+        { firstName: 'דני', lastName: 'שובבני' },
+        { firstName: 'דני', lastName: 'שובבני' },
+        { firstName: 'דני', lastName: 'שובבני' },
+        { firstName: 'דני', lastName: 'שובבני' },
+        { firstName: 'דני', lastName: 'שובבני' },
+        { firstName: 'דני', lastName: 'שובבני' },
+        { firstName: 'דני', lastName: 'שובבני' },
+    ]);
+});
