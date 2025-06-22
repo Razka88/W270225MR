3ce723cab6c68c518bd65e3debc45aead60c57a5
Your Name <you@example.com>
2025-06-22 14:20:58 +0300

    added

--- nodejs/front-for-node/src/Users.jsx
+++ nodejs/front-for-node/src/Users.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 export default function Users() {
     const [users, setUsers] = useState([]);
+    const [isModal, setIsModal] = useState(false);
 
     const getUsers = async () => {
         const res = await fetch("http://localhost:3000/users");
@@ -20,7 +21,28 @@ export default function Users() {
 
     return (
         <div>
-            <h1>Users</h1>
+            <br />
+            {!isModal && <button className="add" onClick={() => setIsModal(true)}>משתמש חדש</button>}
+
+            <h1>משתמשים</h1>
+            {
+                isModal &&
+                <div className="modal">
+                    <button className="close" onClick={() => setIsModal(false)}>X</button>
+
+                    <form>
+                        <label>
+                            שם פרטי
+                            <input type="text" id="firstName" />
+                        </label>
+                        <label>
+                            שם משפחה
+                            <input type="text" id="lastName" />
+                        </label>
+                        <button>הוסף</button>
+                    </form>
+                </div>
+            }
 
             <table>
                 <thead>
