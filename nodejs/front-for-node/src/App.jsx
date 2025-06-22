071cdb6df2310613f3e233965577ff9dae618278
Your Name <you@example.com>
2025-06-22 13:40:28 +0300

    added

--- nodejs/front-for-node/src/App.jsx
+++ nodejs/front-for-node/src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Users from './Users'
 
 function App() {
 
   return (
     <>
-      <h1>שלום לכולם</h1>
+      <Users />
     </>
   )
 }
